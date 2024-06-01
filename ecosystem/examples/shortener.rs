5bd55baab9493448275f1fa74b616b3f433233a9
yc-wang <yicheng.wang.2023@gmail.com>
2024-06-01 22:17:00 +0100

    refactor: refactor shorterner

--- ecosystem/examples/shortener.rs
+++ ecosystem/examples/shortener.rs
@@ -1,28 +1,24 @@
 use anyhow::Result;
 use axum::{
     extract::{Path, State},
-    http::{header, StatusCode},
     response::IntoResponse,
     routing::{get, post},
     Json, Router,
 };
+use http::{header::LOCATION, HeaderMap, StatusCode};
 use nanoid::nanoid;
 use serde::{Deserialize, Serialize};
 use sqlx::{FromRow, PgPool};
 use tokio::net::TcpListener;
-use tracing::{info, level_filters::LevelFilter};
-use tracing_subscriber::{
-    fmt::Layer, layer::SubscriberExt as _, util::SubscriberInitExt as _, Layer as _,
-};
+use tracing::{info, level_filters::LevelFilter, warn};
+use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
 
-const LISTEN_ADDR: &str = "localhost:9876";
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 struct ShortenReq {
     url: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize)]
 struct ShortenRes {
     url: String,
 }
@@ -40,15 +36,16 @@ struct UrlRecord {
     url: String,
 }
 
+const LISTEN_ADDR: &str = "127.0.0.1:9876";
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let layer = Layer::new().pretty().with_filter(LevelFilter::INFO);
+    let layer = Layer::new().with_filter(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
 
-    let url = "postgres://postgres:password@localhost:5432/shortener";
-    let state = AppState::new(url).await?;
-    info!("Connected to database");
-
+    let url = "postgres://postgres:postgres@localhost:5432/shortener";
+    let state = AppState::try_new(url).await?;
+    info!("Connected to database: {url}");
     let listener = TcpListener::bind(LISTEN_ADDR).await?;
     info!("Listening on: {}", LISTEN_ADDR);
 
@@ -66,35 +63,34 @@ async fn shorten(
     State(state): State<AppState>,
     Json(data): Json<ShortenReq>,
 ) -> Result<impl IntoResponse, StatusCode> {
-    let id = state
-        .shorten(&data.url)
-        .await
-        .map_err(|_| StatusCode::UNPROCESSABLE_ENTITY)?;
-
+    let id = state.shorten(&data.url).await.map_err(|e| {
+        warn!("Failed to shorten URL: {e}");
+        StatusCode::UNPROCESSABLE_ENTITY
+    })?;
     let body = Json(ShortenRes {
-        url: format!("{}/{}", LISTEN_ADDR, id),
+        url: format!("http://{}/{}", LISTEN_ADDR, id),
     });
-
     Ok((StatusCode::CREATED, body))
 }
 
 async fn redirect(
-    State(state): State<AppState>,
     Path(id): Path<String>,
+    State(state): State<AppState>,
 ) -> Result<impl IntoResponse, StatusCode> {
-    let url = state.get_url(&id).await.map_err(|_| StatusCode::NOT_FOUND);
+    let url = state
+        .get_url(&id)
+        .await
+        .map_err(|_| StatusCode::NOT_FOUND)?;
 
-    let mut headers = header::HeaderMap::new();
-    headers.insert(header::LOCATION, url.unwrap().parse().unwrap());
-
-    Ok((StatusCode::FOUND, headers))
+    let mut headers = HeaderMap::new();
+    headers.insert(LOCATION, url.parse().unwrap());
+    Ok((StatusCode::PERMANENT_REDIRECT, headers))
 }
 
 impl AppState {
-    async fn new(url: &str) -> Result<Self> {
+    async fn try_new(url: &str) -> Result<Self> {
         let pool = PgPool::connect(url).await?;
-
-        // create table if not exists
+        // Create table if not exists
         sqlx::query(
             r#"
             CREATE TABLE IF NOT EXISTS urls (
@@ -105,29 +101,26 @@ impl AppState {
         )
         .execute(&pool)
         .await?;
-
         Ok(Self { db: pool })
     }
 
     async fn shorten(&self, url: &str) -> Result<String> {
         let id = nanoid!(6);
         let ret: UrlRecord = sqlx::query_as(
-            "INSERT INTO urls (id, url) VALUES ($1, $2) ON CONFLICT(url) DO UPDATE SET URL = EXCLUDED.URL RETURNING id",
+            "INSERT INTO urls (id, url) VALUES ($1, $2) ON CONFLICT(url) DO UPDATE SET url=EXCLUDED.url RETURNING id",
         )
         .bind(&id)
         .bind(url)
         .fetch_one(&self.db)
         .await?;
-
         Ok(ret.id)
     }
 
-    async fn get_url(&self, path: &str) -> Result<String> {
+    async fn get_url(&self, id: &str) -> anyhow::Result<String> {
         let ret: UrlRecord = sqlx::query_as("SELECT url FROM urls WHERE id = $1")
-            .bind(path)
+            .bind(id)
             .fetch_one(&self.db)
             .await?;
-
         Ok(ret.url)
     }
 }
